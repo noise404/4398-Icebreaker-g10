33b0adc0c521b3eaaa7279b949dbbe1372a5c4d2
tuj91536 <tuj91536@temple.edu>
2023-08-28 21:10:05 -0400

    gets data from syllabus through api call

--- api_calls.py
+++ api_calls.py
@@ -1,7 +1,17 @@
 # 3sSgKx00ZdDvrU7QYkiy0SKek4uUjz1HkrFZPP84
 import requests
+import datetime
 
+current_date = datetime.datetime.today().strftime("%Y-%m-%d")
+# example of math with dates 
+current_date = datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(days=1)
+print(current_date)
 
-user_request = requests.get("https://courses.ianapplebaum.com/api/user", headers={"Authorization":"Bearer 3sSgKx00ZdDvrU7QYkiy0SKek4uUjz1HkrFZPP84"})
+headers = {
+    "Authorization": "Bearer 3sSgKx00ZdDvrU7QYkiy0SKek4uUjz1HkrFZPP84",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
+user_request = requests.get("https://courses.ianapplebaum.com/api/syllabus/2", headers=headers)
 
 print(user_request.json())
