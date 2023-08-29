a0d46049079c2e4993badb74f9ef893564f39b9b
Alex Rajasekaran <50925468+triplehamburger@users.noreply.github.com>
2023-08-29 18:25:23 -0400

    Sends name and date notification for the next 3 assignments

--- app.py
+++ app.py
@@ -5,26 +5,21 @@ import datetime
 current_date = datetime.datetime.today().strftime("%Y-%m-%d")
 # example of math with dates 
 current_date = datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(days=1)
-print(current_date)
+#print(current_date)
 
 headers = {
     "Authorization": "Bearer 3sSgKx00ZdDvrU7QYkiy0SKek4uUjz1HkrFZPP84",
     "Content-Type": "application/json",
     "Accept": "application/json",
 }
-user_request = requests.get("https://courses.ianapplebaum.com/api/syllabus/2", headers=headers)
-
-#print(user_request.json())
+user_request = requests.get("https://courses.ianapplebaum.com/api/syllabus/4", headers=headers)
 
 data = user_request.json()
 events = data.get('events', [])
 
-# for event in events:
-#     event_name = event.get('event_name')
-#     event_date = event.get('event_date')
-#     print(event_name + " ---- " + event_date)
+#Iterate through syllabus and build string of assignment and date
 
-NotifyEventList = []
+MessageString = ""
 counter = 0
 for event in events:
     event_name = event.get('event_name')
@@ -32,16 +27,14 @@ for event in events:
     event_date_obj = datetime.datetime.strptime(event_date, "%Y-%m-%d")
     if(event_date_obj < current_date):
         continue
-    NotifyEventList.add((event_name, event_date_obj))
+    MessageString += event_name + " " + event_date + "\n"
     counter += 1
     if(counter >= 3):
         break
-    # print(event_name + " ---- " + event_date)
 
-print(NotifyEventList)
+#Send notification
 
 toast = Notification(app_id="CIS 4398 Due Dates Reminder",
                      title="Next Three Assignments",
-                     msg= NotifyEventList)
-
+                     msg= MessageString)
 toast.show()
