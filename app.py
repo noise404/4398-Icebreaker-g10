359fa6eaa73be63aac45fdfa74dff7abd53dd381
Alex Rajasekaran <50925468+triplehamburger@users.noreply.github.com>
2023-08-30 13:15:24 -0400

    made the notification look nicer

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import datetime
 current_date = datetime.datetime.today().strftime("%Y-%m-%d")
 # example of math with dates 
 current_date = datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(days=1)
-#print(current_date)
+
+# Request JSON
 
 headers = {
     "Authorization": "Bearer 3sSgKx00ZdDvrU7QYkiy0SKek4uUjz1HkrFZPP84",
@@ -27,7 +28,7 @@ for event in events:
     event_date_obj = datetime.datetime.strptime(event_date, "%Y-%m-%d")
     if(event_date_obj < current_date):
         continue
-    MessageString += event_name + " " + event_date + "\n"
+    MessageString += ">" + event_name + " - " + event_date + "\n"
     counter += 1
     if(counter >= 3):
         break
